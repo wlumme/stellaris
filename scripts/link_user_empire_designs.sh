c0f011fedb8b9091b46ce52ff86ccd84f4aa0c95
William Lumme <80401792+wlumme@users.noreply.github.com>
2026-02-17 19:42:24 +0000

    Replace symbolic links with hard links

--- scripts/link_user_empire_designs.sh
+++ scripts/link_user_empire_designs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ln --force --relative --symbolic ~/.local/share/Paradox\ Interactive/Stellaris/user_empire_designs_*.txt ./user_empire_designs/ 
+ln --force ~/.local/share/Paradox\ Interactive/Stellaris/user_empire_designs_*.txt ./user_empire_designs/
